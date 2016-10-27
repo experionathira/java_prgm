4adfd3278a73bca1f5b73b91b6b101f6e5003971
athira <you@example.com>
2016-10-27 13:02:46 +0530

    Initial commit

---
+++ factorial.java
@@ -0,0 +1,24 @@
+import.java.io;
+
+class factorial{
+		
+		public static void main()
+			{
+				int i,n;
+				int fact=1;
+				for(i=1;i<=n;i++)
+				{
+					fact=i*fact;
+
+				}
+	`			System.out.println("factorial=" fact);
+			}
+
+
+
+
+
+
+
+}
+
